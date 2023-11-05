e987aacd696455129c2c2f6a5d102cae3cf94c1d
Kuroyia <williamtran@outlook.fr>
2023-11-05 23:53:34 +0100

    fruits and pause

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ from pacman import Pacman
 from nodes import NodeGroup
 from pellets import PelletGroup
 from ghosts import GhostGroup
+from fruit import Fruit
+from pauser import Pause
 
 
 class GameController(object):
@@ -13,6 +15,8 @@ class GameController(object):
         self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
         self.background = None
         self.clock = pygame.time.Clock()
+        self.fruit = None
+        self.pause = Pause(True)
 
     def setBackground(self):
         self.background = pygame.surface.Surface(SCREENSIZE).convert()
@@ -36,11 +40,19 @@ class GameController(object):
 
     def update(self):
         dt = self.clock.tick(60) / 1000.0
-        self.pacman.update(dt)
-        self.ghosts.update(dt)
+
         self.pellets.update(dt)
-        self.checkPelletEvents()
-        self.checkGhostEvents()
+        if not self.pause.paused:
+            self.pacman.update(dt)
+            self.ghosts.update(dt)
+            if self.fruit is not None:
+                self.fruit.update(dt)
+            self.checkPelletEvents()
+            self.checkGhostEvents()
+            self.checkFruitEvents()
+        afterPauseMethod = self.pause.update(dt)
+        if afterPauseMethod is not None:
+            afterPauseMethod()
         self.checkEvents()
         self.render()
 
@@ -48,6 +60,13 @@ class GameController(object):
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    self.pause.setPause(playerPaused=True)
+                    if not self.pause.paused:
+                        self.showEntities()
+                    else:
+                        self.hideEntities()
 
     def checkPelletEvents(self):
         pellet = self.pacman.eatPellets(self.pellets.pelletList)
@@ -61,12 +80,35 @@ class GameController(object):
         for ghost in self.ghosts:
             if self.pacman.collideGhost(ghost):
                 if ghost.mode.current is FREIGHT:
+                    self.pacman.visible = False
+                    ghost.visible = False
+                    self.pause.setPause(pauseTime=1, func=self.showEntities)
                     ghost.startSpawn()
+
+    def showEntities(self):
+        self.pacman.visible = True
+        self.ghosts.show()
+
+    def hideEntities(self):
+        self.pacman.visible = False
+        self.ghosts.hide()
+
+    def checkFruitEvents(self):
+        if self.pellets.numEaten == 50 or self.pellets.numEaten == 140:
+            if self.fruit is None:
+                self.fruit = Fruit(self.nodes.getNodeFromTiles(9, 20))
+        if self.fruit is not None:
+            if self.pacman.collideCheck(self.fruit):
+                self.fruit = None
+            elif self.fruit.destroy:
+                self.fruit = None
 
     def render(self):
         self.screen.blit(self.background, (0, 0))
         self.nodes.render(self.screen)
         self.pellets.render(self.screen)
+        if self.fruit is not None:
+            self.fruit.render(self.screen)
         self.pacman.render(self.screen)
         self.ghosts.render(self.screen)
         pygame.display.update()
